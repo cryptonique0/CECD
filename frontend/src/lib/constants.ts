0bde734868c6bc5f8de079290bed169620766a6e
cryptonique0 <abdulganiyu838@gmail.com>
2025-12-02 23:33:48 +0100

    feat(config): Add blockchain constants for Celo networks and tokens

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,28 @@
+export const CELO_MAINNET = {
+  chainId: 42220,
+  name: 'Celo',
+  rpcUrl: 'https://forno.celo.org',
+  blockExplorer: 'https://explorer.celo.org',
+  nativeCurrency: {
+    name: 'CELO',
+    symbol: 'CELO',
+    decimals: 18,
+  },
+};
+
+export const CELO_ALFAJORES = {
+  chainId: 44787,
+  name: 'Alfajores Testnet',
+  rpcUrl: 'https://alfajores-forno.celo-testnet.org',
+  blockExplorer: 'https://alfajores.celoscan.io',
+  nativeCurrency: {
+    name: 'CELO',
+    symbol: 'CELO',
+    decimals: 18,
+  },
+};
+
+export const TOKEN_ADDRESSES = {
+  cUSD: '0x765DE816845861e75A25fCA122bb6898B8B1282a',
+  cEUR: '0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73',
+};
